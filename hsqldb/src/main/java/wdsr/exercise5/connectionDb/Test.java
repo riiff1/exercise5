6815c1c23bc553ebc5620b7b4f01327481dfa4e4
riiff1 <riiff@wp.pl>
2017-05-18 21:05:19 +0200

    test commit

---
+++ hsqldb/src/main/java/wdsr/exercise5/connectionDb/Test.java
@@ -0,0 +1,10 @@
+package wdsr.exercise5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import wdsr.exercise5.hsqldb.MyHsqlServer;
+
+public class Test {
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+}
